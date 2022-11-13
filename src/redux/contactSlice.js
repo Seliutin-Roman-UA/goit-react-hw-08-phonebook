8931b79d0ffa3ded989a0678ed63f3f61b74e921
Seliutin-Roman-UA <selutin@gmail.com>
2022-11-14 01:17:37 +0200

    Update contactSlice.js

--- src/redux/contactSlice.js
+++ src/redux/contactSlice.js
@@ -8,7 +8,7 @@ let initcontact = [
   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
 ];
-initcontact = JSON.parse(localStorage.getItem(keyLS)) ?? initcontact;
+
 
 const contactSlice = createSlice({
   name: 'contacts',
