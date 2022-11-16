659af9f0234d35409b45addca12b3acfa86a7037
Seliutin-Roman-UA <selutin@gmail.com>
2022-11-16 03:13:28 +0200

    final

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,13 +3,15 @@ import { Filter } from './Filter/Filter';
 import { ListContacts } from './ListContacts/ListContacts';
 import { PhoneBook } from './App.styled';
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { getContacts } from 'redux/operations';
 
 export function App() {
-
   const dispatch = useDispatch();
-  
+  const isLoading = useSelector(state => state.contacts.isLoading);
+  const error = useSelector(state => state.contacts.error);
+ 
+
   useEffect(() => {
     dispatch(getContacts());
   }, [dispatch]);
@@ -18,6 +20,7 @@ export function App() {
     <PhoneBook>
       <h1>Phonebook</h1>
       <ContactForm />
+      {isLoading && !error && 'Waiting, data is loading'}
       <h2>Contacts</h2>
       <Filter />
       <ListContacts />
